7b81444b61eb826cacbc2e803f8923e512a4d4f2
Aleksander Matusiak <aleks.matusiak@hotmail.com>
2020-01-22 21:42:35 +0100

    added plan for base funcs

--- lib.h
+++ lib.h
@@ -1,4 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Definicje
+//Deklaracje
+
+//Pierwsze wersje są void typ void
+
+//Pokaz menu
+void showMenu(void);
+
+//Pokaz plansze 
+void printBoard(void);
+
+//Wrzuc krazek
+void move(void);
+
+//Sprawdzanie zwyciestwa
+void checkWin(void);
