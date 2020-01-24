701017a8ae311976686be526f0c371787b4263b1
dvnDC <cichosz-damian@wp.pl>
2020-01-24 01:31:52 +0100

    Update lib.c
    zabezpieczenie przed wyjsciem z kolumny, tekst pod plansza

--- lib.c
+++ lib.c
@@ -84,10 +84,183 @@ void freeBoard(Game *game){
     free(game);
 }
 
+#include "lib.h"
+
+//Funkcje
+void setDefaultBoard(Game *game){
+    game->columns = 7;
+    game->rows = 6;
+}
+
+/*void changeDefaultBoard(void){
+
+    printf("Domyślna liczba kolumn to 7. Podaj nową ilość kolumn:");
+    scanf("%d", &defaultColumn);
+    putchar('\n');
+    printf("Domyślna liczba wierszy to 6. Podaj nową ilość wierszy:");
+    scanf("%d", &defaultRow);
+    putchar('\n');
+}*/
+
+void createBoard (Game *game){
+
+    game->board= (int**)malloc(game->rows * sizeof(int*));
+    if (game->board == NULL){
+        perror("Error in malloc");
+        exit (1);
+    }
+    
+    int i;
+
+    for (i = 0; i < game->rows; i++){
+        game->board[i] = (int*)malloc(game->columns * sizeof(int));
+        if(game->board[i] == NULL){
+            perror("Error in second malloc");
+            exit (1);
+        }
+    }
+}
+
+void printBoard(Game *game){
+
+    int i, j;
+
+    //Na podstawie obrazka z zadania z https://www.it2051229.com/cconn4.html
+    for (j = 0; j < game->columns; j++){
+            printf(" %d  ", j+1);
+        }
+
+    printf ("\n");
+
+    for (i = 0; i < game->rows; i++) {
+        printf ("+");
+        for (j = 0; j < game->columns; j++){
+            printf("---+");
+        }
+        printf ("\n");
+        printf ("|");
+        for (j = 0; j < game->columns; j++){
+            if(game->board[i][j]==0)
+                printf("   |");
+            else 
+            printf(" %d |", game->board[i][j]);
+        }
+        printf ("\n");
+    }
+
+    printf ("+");
+    for (j = 0; j < game->columns; j++){
+        printf("---+");
+    }
+
+    printf ("\n");
+    
+}
+
+void freeBoard(Game *game){
+
+    //naprawić dealokację
+
+    int i;
+
+    for (i = 0; i < game->rows; i++){
+            free(game->board[i]);
+        }
+    free(game->board);
+    free(game);
+}
+
+#include "lib.h"
+
+//Funkcje
+void setDefaultBoard(Game *game){
+    game->columns = 7;
+    game->rows = 6;
+}
+
+/*void changeDefaultBoard(void){
+
+    printf("Domyślna liczba kolumn to 7. Podaj nową ilość kolumn:");
+    scanf("%d", &defaultColumn);
+    putchar('\n');
+    printf("Domyślna liczba wierszy to 6. Podaj nową ilość wierszy:");
+    scanf("%d", &defaultRow);
+    putchar('\n');
+}*/
+
+void createBoard (Game *game){
+
+    game->board= (int**)malloc(game->rows * sizeof(int*));
+    if (game->board == NULL){
+        perror("Error in malloc");
+        exit (1);
+    }
+    
+    int i;
+
+    for (i = 0; i < game->rows; i++){
+        game->board[i] = (int*)malloc(game->columns * sizeof(int));
+        if(game->board[i] == NULL){
+            perror("Error in second malloc");
+            exit (1);
+        }
+    }
+}
+
+void printBoard(Game *game){
+
+    int i, j;
+
+    //Na podstawie obrazka z zadania z https://www.it2051229.com/cconn4.html
+    for (j = 0; j < game->columns; j++){
+            printf(" %d  ", j+1);
+        }
+
+    printf ("\n");
+
+    for (i = 0; i < game->rows; i++) {
+        printf ("+");
+        for (j = 0; j < game->columns; j++){
+            printf("---+");
+        }
+        printf ("\n");
+        printf ("|");
+        for (j = 0; j < game->columns; j++){
+            if(game->board[i][j]==0)
+                printf("   |");
+            else 
+            printf(" %d |", game->board[i][j]);
+        }
+        printf ("\n");
+    }
+
+    printf ("+");
+    for (j = 0; j < game->columns; j++){
+        printf("---+");
+    }
+
+    printf ("\n");
+    
+}
+
+void freeBoard(Game *game){
+
+    //naprawić dealokację
+
+    int i;
+
+    for (i = 0; i < game->rows; i++){
+            free(game->board[i]);
+        }
+    free(game->board);
+    free(game);
+}
+
 int move(Game *game){
     int chooseColumn;
     while(1){
     int checkRow=game->rows-1; 
+    int msg=0;
     printf("\nChoose number: ");
     scanf("%d",&chooseColumn);
     while(game->columns<chooseColumn && chooseColumn>0){
@@ -95,10 +268,17 @@ int move(Game *game){
         scanf("%d",&chooseColumn);
     }
     while(game->board[checkRow][chooseColumn-1]==1){
+        if(checkRow==0){
+            msg=1;
+            break;
+        }
         checkRow--;
+        
     }
     game->board[checkRow][chooseColumn-1]=1;
     printBoard(game);
+    if(msg==1)
+    printf("\nThis column is full, sorry! Choose another one. :)\n");
     }
 
 }
