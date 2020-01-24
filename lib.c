fe59368ea3e174128491c514f12cf615064cab35
dvnDC <cichosz-damian@wp.pl>
2020-01-24 03:34:10 +0100

    Update lib.c
    Zrobione sprawdzanie w poziomie. Przetestowałem dość dobrze (różne poziomy) i działało dla dwóch graczy. Tam jest juz zaczęte sprawdzanie w pionie -> póki co skomentowałem. Łopatologicznie zmienne ustawiłem, żeby było mi wygodnie, jak masz pomysł, żeby je ograniczyć to śmiało. :)

--- lib.c
+++ lib.c
@@ -4,7 +4,6 @@
 void setDefaultBoard(Game *game){
     game->columns = 7;
     game->rows = 6;
-    game->player = Player1;
 }
 
 /*void changeDefaultBoard(void){
@@ -72,16 +71,6 @@ void printBoard(Game *game){
     
 }
 
-void switchPlayers(Game *game){
-    if (game->player == Player1){
-        game->player = Player2;
-    }
-    else
-    {
-        game->player = Player1;
-    }
-}
-
 void freeBoard(Game *game){
 
     //naprawić dealokację
@@ -95,35 +84,101 @@ void freeBoard(Game *game){
     free(game);
 }
 
+int checkColumns(Game *game){
+    int countColumnsP1=0;
+    int countColumnsP2=0;
+    int rows=game->rows-1;
+    int columns=game->columns-1;
+    
+    int win;
+    for(rows; 0<=rows; rows--){                     //zle, rows od dolu pownnio byc><
+        for(int i=0; i<=columns; i++){
+            if(game->board[rows][i]==1)
+            countColumnsP1++;
+            else
+            countColumnsP1=0;
+                if(countColumnsP1==4){
+                    win=1;
+                    break;
+                }
+                
+            if(game->board[rows][i]==2)
+            countColumnsP2++;
+            else
+            countColumnsP2=0;
+                if(countColumnsP2==4){
+                    win=2;
+                    break;
+                }    
+            
+        }
+        // if(game->board[rows][0]==1)
+        //     countColumnsP1++;
+        //         if(countColumnsP1==4){
+        //             win=1;
+        //             break;
+        //         }
+        if(win==1||win==2)
+        break;
+    }
+    return win;
+}
+
 void move(Game *game){
     int chooseColumn;
     int player=1;
+    int count=game->columns*game->rows;
+    int win;
+
     while(1){
-    int checkRow=game->rows-1; 
-    int msg=0;
-    printf("\nChoose number: ");
-    scanf("%d",&chooseColumn);
-    while(game->columns<chooseColumn && chooseColumn>0){
-        printf("\nWrong number! Try again: %d",game->columns);
+        int checkRow=game->rows-1; 
+        int msg=0;
+        printf("\nChoose number: ");
         scanf("%d",&chooseColumn);
-    }
-    while(game->board[checkRow][chooseColumn-1]==1||game->board[checkRow][chooseColumn-1]==2){
-        if(checkRow==0){
-            msg=1;
+        while(game->columns<chooseColumn || chooseColumn<=0){
+             printf("\nWrong number! Try again: ");
+             scanf("%d",&chooseColumn);
+         }
+        while(game->board[checkRow][chooseColumn-1]==1||game->board[checkRow][chooseColumn-1]==2){
+            if(checkRow==0){
+                msg=1;
+                break;
+            }
+            checkRow--;
+        
+        }
+        if(msg==1){
+            printBoard(game);
+            printf("\nThis column is full, sorry! Choose another one. :)\n");
+        }
+        else{
+            game->board[checkRow][chooseColumn-1]=player;
+            printBoard(game);
+            count--;
+            if(player==1) 
+            player=2;
+            else
+            player--;
+        }
+    
+        
+    
+    
+        if(count==0){
+            printf("===========================\nIt seems that we got draw!\n===========================\n");
             break;
         }
-        checkRow--;
-        
+        win=checkColumns(game);
+        if(win==1){
+            printf("\nPLAYER 1 WON!!!!!\n\n\n");
+            break;
+        }
+        else if(win==2){
+            printf("\nPLAYER 2 WON!!!!!\n\n\n");
+            break;
+        }
     }
-    game->board[checkRow][chooseColumn-1]=player;
-    printBoard(game);
-    if(msg==1)
-    printf("\nThis column is full, sorry! Choose another one. :)\n");
-    if(player==1)
-        player=2;
-    else
-        player--;
-    }
+    
     
 }
 
